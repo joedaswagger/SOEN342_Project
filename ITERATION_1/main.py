2e823e9febe68961a2219ad3b09214d9b6605dda
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-10-03 13:17:14 -0400

    Create route objects within the parser.

---
+++ ITERATION_1/main.py
@@ -0,0 +1,3 @@
+from utils.route_parser import Route_parser
+
+parser = Route_parser(False, "../data/eu_rail_network.csv")
--- ITERATION_1/utils/route_parser.py
+++ ITERATION_1/utils/route_parser.py
@@ -1,9 +1,12 @@
 import csv
 import os
-from route import Route
+from utils.route import Route
+# import tkinter as tk
+# from tkinter import filedialog
 
 class Route_parser:
-    def __init__(self, relative_path):
+    def __init__(self, default, relative_path):
+        self.default = default
         self.relative_path = relative_path
         self.filepath = None
         self.file = None
@@ -13,11 +16,16 @@ class Route_parser:
         self.read_csv()
 
     def build_path(self):
+        # root = tk.Tk()
+        # root.withdraw()
+        # x = filedialog.askopenfilename()
         absolute_path = os.path.dirname(__file__)
         self.filepath = os.path.join(absolute_path, self.relative_path)
 
     def read_csv(self):
         with open(self.filepath, mode ="r", encoding="utf8")as csv_file:
             self.file = csv.reader(csv_file)
+            next(self.file)
             for line in self.file:
-                print(line)
+                route = Route(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8])
+                self.routes.append(route)
