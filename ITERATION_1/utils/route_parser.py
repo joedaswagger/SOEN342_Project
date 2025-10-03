dd8ff54657560ca01e376146c51c0c93cc8682fe
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-10-03 12:44:19 -0400

    Create functions for CSV reading.

--- ITERATION_1/utils/route_parser.py
+++ ITERATION_1/utils/route_parser.py
@@ -1,5 +1,23 @@
+import csv
+import os
+from route import Route
+
 class Route_parser:
-    def __init__(self, filepath, file, routes):
-        self.filepath = filepath
+    def __init__(self, relative_path):
+        self.relative_path = relative_path
+        self.filepath = None
         self.file = None
-        self.routes = routes
+        self.routes = []
+
+        self.build_path()
+        self.read_csv()
+
+    def build_path(self):
+        absolute_path = os.path.dirname(__file__)
+        self.filepath = os.path.join(absolute_path, self.relative_path)
+
+    def read_csv(self):
+        with open(self.filepath, mode ="r", encoding="utf8")as csv_file:
+            self.file = csv.reader(csv_file)
+            for line in self.file:
+                print(line)
