18014f344a6259b1fd5c55696873e41d37d53702
s <balls@balls.com>
2025-10-24 20:34:07 -0400

    Added cost calculation

--- ITERATION_2/models/trip.py
+++ ITERATION_2/models/trip.py
@@ -1,12 +1,12 @@
 from models.ticket import Ticket
 
 class Trip:
-    def __init__(self, trip_id, trip_type, travelling_class, client_id):
+    def __init__(self, trip_id, trip_type, travelling_class, total_cost, client_id):
         self.trip_id = trip_id
         self.tickets = []
         self.trip_type = trip_type
         self.travelling_class = travelling_class
-        self.total_cost = 0
+        self.total_cost = total_cost
         self.client_id = client_id
 
     def add_ticket(self, ticket: Ticket):
@@ -14,3 +14,6 @@ class Trip:
 
     def set_trip_type(self, type):
         self.trip_type = type
+
+    def set_total_cost(self, counter):
+        self.total_cost *= counter
